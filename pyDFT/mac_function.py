7573da40ef99258c657c9a31f959ae5b684b915b
mac <macauleycurtis@hotmail.co.uk>
2022-05-20 18:51:20 +0100

    Commiting the black myfile.py version

--- pyDFT/mac_function.py
+++ pyDFT/mac_function.py
@@ -1,23 +1,19 @@
-
-
 def roman_numeral(s: str) -> int:
-    """ Takes a roman numeral, entered as 's' a string, and returns the corresponding
+    """Takes a roman numeral, entered as 's' a string, and returns the corresponding
     value as an integer
 
 
     """
-    if s == 'I':
+    if s == "I":
         return 1
-    if s == 'IV':
+    if s == "IV":
         return 4
-    if s == 'V':
+    if s == "V":
         return 5
-    if s == 'X':
+    if s == "X":
         return 10
 
     if not isinstance(s, str):
         raise ValueError(f"x must be a string; {s} is invalid")
-    while s != 'I' and s !='IV' and s!= 'V' and s != 'X':
+    while s != "I" and s != "IV" and s != "V" and s != "X":
         raise ValueError(f"Invalid choice of numeral, must be I, IV, V or X")
-
-
